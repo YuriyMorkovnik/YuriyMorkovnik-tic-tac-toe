bad7d8adfd1d1f4fd6b7df803bc48822b61400e6
YuriyMorkovnik <yuriymork@yandex.ru>
2020-04-11 13:58:13 +0300

    Updates

---
+++ precache-manifest.f7515643260eccf86edcadc9c4de6e5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce063cd3dedbf2f847dbbaec5addaa2",
+    "url": "/YuriyMorkovnik-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "d09b1bf88aba27384921",
+    "url": "/YuriyMorkovnik-tic-tac-toe/static/css/main.ff0ec109.chunk.css"
+  },
+  {
+    "revision": "17d5390c5d9a624afd3e",
+    "url": "/YuriyMorkovnik-tic-tac-toe/static/js/2.1061c4bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/YuriyMorkovnik-tic-tac-toe/static/js/2.1061c4bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d09b1bf88aba27384921",
+    "url": "/YuriyMorkovnik-tic-tac-toe/static/js/main.038b4163.chunk.js"
+  },
+  {
+    "revision": "2c1f0541f9296d25866c",
+    "url": "/YuriyMorkovnik-tic-tac-toe/static/js/runtime-main.daa71bdd.js"
+  }
+]);
